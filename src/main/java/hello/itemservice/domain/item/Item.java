69a1ef356241d41441840ffb4f794748fd4a2ac7
seonhyeokjun <tjsrmatn@naver.com>
2023-03-11 22:48:17 +0900

    Bean Validation - 시작

--- src/main/java/hello/itemservice/domain/item/Item.java
+++ src/main/java/hello/itemservice/domain/item/Item.java
@@ -1,14 +1,26 @@
 package hello.itemservice.domain.item;
 
 import lombok.Data;
+import org.hibernate.validator.constraints.Range;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import java.util.*;
 
 @Data
 public class Item {
     private Long id;
+
+    @NotBlank
     private String itemName;
+
+    @NotNull
+    @Range(min = 1000, max = 1000000)
     private Integer price;
+
+    @NotNull
+    @Max(9999)
     private Integer quantity;
 
     private Boolean open; // 판매 여부
