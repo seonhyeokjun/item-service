0faac1ce2b1ea6a8fa9625b8ac7b0fce8c66a248
seonhyeokjun <tjsrmatn@naver.com>
2023-03-22 22:48:17 +0900

    홈 화면

--- src/main/java/hello/itemservice/web/HomeController.java
+++ src/main/java/hello/itemservice/web/HomeController.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class HomeController {
     @GetMapping("/")
     public String home() {
-        return "redirect:/items";
+        return "home";
     }
 }
