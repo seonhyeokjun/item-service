26a975397615af7898d83e817738d270ad7bfc29
seonhyeokjun <tjsrmatn@naver.com>
2023-07-09 14:05:05 +0900

    서블릿 필터 - 요청 로그

---
+++ src/main/java/hello/itemservice/web/login/WebConfig.java
@@ -0,0 +1,20 @@
+package hello.itemservice.web.login;
+
+import hello.itemservice.web.filter.LogFilter;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.servlet.Filter;
+
+@Configuration
+public class WebConfig {
+    @Bean
+    public FilterRegistrationBean loginInterceptor() {
+        FilterRegistrationBean<Filter> registrationBean = new FilterRegistrationBean<>();
+        registrationBean.setFilter(new LogFilter());
+        registrationBean.setOrder(1);
+        registrationBean.addUrlPatterns("/*");
+        return registrationBean;
+    }
+}
