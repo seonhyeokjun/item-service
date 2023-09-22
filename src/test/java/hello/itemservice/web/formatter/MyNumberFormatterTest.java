e852e7e735ebdad99e12522bffd139e9e1252c1c
seonhyeokjun <tjsrmatn@naver.com>
2023-09-22 22:48:17 +0900

    포맷터 - Formatter

---
+++ src/test/java/hello/itemservice/web/formatter/MyNumberFormatterTest.java
@@ -0,0 +1,24 @@
+package hello.itemservice.web.formatter;
+
+import org.junit.jupiter.api.Test;
+
+import java.text.ParseException;
+import java.util.Locale;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class MyNumberFormatterTest {
+    MyNumberFormatter formatter = new MyNumberFormatter();
+    @Test
+    void parse() throws ParseException {
+        Number result = formatter.parse("1,000", Locale.KOREA);
+        assertThat(result).isEqualTo(1000L);
+    }
+
+    @Test
+    void print() {
+        String result = formatter.print(1000, Locale.KOREA);
+        assertThat(result).isEqualTo("1,000");
+    }
+}
